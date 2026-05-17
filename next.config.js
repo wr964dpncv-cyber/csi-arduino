228ba0814a01ac7bc0b774c6cd384a19e47ada12
Jonathan <jonathan@amholdings.me>
2026-05-17 03:02:51 -0400

    Redirect /inicio -> / (permanente)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      { source: "/inicio", destination: "/", permanent: true },
+    ];
+  },
 };
 
 module.exports = nextConfig;
